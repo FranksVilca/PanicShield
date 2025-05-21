711054fcb8f2863c79d3296b128a86ea40b2a933
Usuario25A <aanazcoh@gmail.com>
2025-05-20 18:37:25 -0600

    Pantallas

---
+++ app/src/main/java/com/example/panicshield/ui/screen/ContactScreen.kt
@@ -0,0 +1,25 @@
+package com.tu_paquete.ui.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ContactsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Contactos")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewContactsScreen() {
+    ContactsScreen()
+}
